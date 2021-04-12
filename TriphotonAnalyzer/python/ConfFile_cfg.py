1409196343685afcdc114ab5879e145085cd6db3
Uzziel <uzzie.perez@cern.ch>
2021-04-12 12:20:07 -0500

    change dir

--- TriphotonAnalyzer/python/ConfFile_cfg.py
+++ TriphotonAnalyzer/python/ConfFile_cfg.py
@@ -75,8 +75,8 @@ process.TFileService = cms.Service("TFileService",
 # EGM Tools
 #
 ################
-from EgammaUser.EgammaPostRecoTools import setupEgammaPostRecoSeq
-#from RecoEgamma.EgammaTools.EgammaPostRecoTools import setupEgammaPostRecoSeq
+# from EgammaUser.EgammaPostRecoTools import setupEgammaPostRecoSeq
+from RecoEgamma.EgammaTools.EgammaPostRecoTools import setupEgammaPostRecoSeq
 setupEgammaPostRecoSeq(process,
                        runVID=True,
                        #era='2018-Prompt',
